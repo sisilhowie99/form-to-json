daef259bf06b535f8ed4acc779d7f0d715cc3fb8
sisilhowie99 <sisil.anastasia@gmail.com>
2025-04-02 01:50:23 +1000

    add itinerary functionalities
    * rename formData.productItineraries to formData.productItinerary
    * add ability to add & remove itinerary items
    * add ability to select & display itinerary icons
    * add ability to update itinerary image and display it
    * update fill form to fill images
    * update next.config.ts to allow external image fetching

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  // https://nextjs.org/docs/pages/api-reference/config/next-config-js
+  images: {
+    // https://nextjs.org/docs/pages/api-reference/components/image#remotepatterns
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "**",
+        search: "",
+      },
+    ],
+  },
+}
 
 export default nextConfig;
--- src/app/components/Form/index.tsx
+++ src/app/components/Form/index.tsx
@@ -5,12 +5,20 @@ import Box from "@mui/material/Box"
 import Button from "@mui/material/Button"
 import FormControl from "@mui/material/FormControl"
 import Grid from "@mui/material/Grid"
+import InputAdornment from "@mui/material/InputAdornment"
 import InputLabel from "@mui/material/InputLabel"
 import MenuItem from "@mui/material/MenuItem"
 import Select, { SelectChangeEvent } from "@mui/material/Select"
 import Stack from "@mui/material/Stack"
 import TextField from "@mui/material/TextField"
 import Typography from "@mui/material/Typography"
+import DeleteIcon from "@mui/icons-material/Delete"
+import DirectionsBusIcon from "@mui/icons-material/DirectionsBus"
+import AirplanemodeActiveIcon from "@mui/icons-material/AirplanemodeActive"
+import DirectionsBoatIcon from "@mui/icons-material/DirectionsBoat"
+import HotelIcon from "@mui/icons-material/Hotel"
+import BeachAccessIcon from "@mui/icons-material/BeachAccess"
+import Image from "next/image"
 
 enum PRODUCT_TYPES {
   CRUISE = "Cruise",
@@ -20,11 +28,19 @@ enum PRODUCT_TYPES {
   HOLIDAY_PACKAGE = "Holiday Package",
 }
 
+const PRODUCT_ICONS = {
+  [PRODUCT_TYPES.CRUISE.toLowerCase()]: <DirectionsBoatIcon />,
+  [PRODUCT_TYPES.TOUR.toLowerCase()]: <DirectionsBusIcon />,
+  [PRODUCT_TYPES.ACCOMMODATION.toLowerCase()]: <HotelIcon />,
+  [PRODUCT_TYPES.FLIGHT.toLowerCase()]: <AirplanemodeActiveIcon />,
+  [PRODUCT_TYPES.HOLIDAY_PACKAGE.toLowerCase()]: <BeachAccessIcon />,
+}
+
 interface FormData {
   productName: string
   productType: string
   productPrice: string
-  productItineraries: Itinerary
+  productItinerary: Itinerary
   productInclusions: string[]
   productExclusions: string[]
 }
@@ -47,7 +63,7 @@ export const Form = () => {
     productName: "",
     productType: "",
     productPrice: "",
-    productItineraries: {
+    productItinerary: {
       summary: "",
       image: "",
       itineraries: [],
@@ -82,7 +98,7 @@ export const Form = () => {
       productName: "",
       productType: "",
       productPrice: "",
-      productItineraries: {
+      productItinerary: {
         summary: "",
         image: "",
         itineraries: [],
@@ -96,20 +112,20 @@ export const Form = () => {
       productName: "Product XYZ",
       productType: PRODUCT_TYPES.CRUISE,
       productPrice: "1000",
-      productItineraries: {
+      productItinerary: {
         summary: "",
-        image: "",
+        image: "https://picsum.photos/id/49/1280/792",
         itineraries: [
           {
             day: 1,
-            icon: "https://example.com/icon.png",
-            image: "https://example.com/image.png",
+            icon: "cruise",
+            image: "https://picsum.photos/id/11/2500/1667",
             content: "Day 1 content",
           },
           {
             day: 2,
-            icon: "https://example.com/icon.png",
-            image: "https://example.com/image.png",
+            icon: "accommodation",
+            image: "https://picsum.photos/200",
             content: "Day 2 content",
           },
         ],
@@ -117,6 +133,88 @@ export const Form = () => {
       productInclusions: [],
       productExclusions: [],
     })
+
+  const addItinerary = () => {
+    const newItinerary: ItineraryItem = {
+      day: formData.productItinerary?.itineraries
+        ? formData.productItinerary?.itineraries.length + 1
+        : 1,
+      icon: "",
+      image: "",
+      content: "",
+    }
+    setFormData({
+      ...formData,
+      productItinerary: {
+        ...formData.productItinerary,
+        itineraries: [
+          ...(formData.productItinerary?.itineraries ?? []),
+          newItinerary,
+        ],
+      },
+    })
+  }
+
+  const removeItinerary = (day: number) => {
+    const filteredItineraries = formData.productItinerary?.itineraries?.filter(
+      (itinerary) => itinerary.day !== day
+    )
+
+    // update the day numbers of the remaining itineraries
+    const updatedItineraries = filteredItineraries?.map((itinerary, index) => ({
+      ...itinerary,
+      day: index + 1,
+    }))
+
+    setFormData({
+      ...formData,
+      productItinerary: {
+        ...formData.productItinerary,
+        itineraries: updatedItineraries,
+      },
+    })
+  }
+
+  const handleItineraryChange = (
+    e: ChangeEvent<HTMLInputElement> | SelectChangeEvent<string>
+  ) => {
+    const { name, value } = e.target
+    const [type, day] = name.split("-") // Split the name to get the type and day number
+    const parsedDay = parseInt(day, 10) // Extract the day number from the name
+    const targetItinerary = formData.productItinerary?.itineraries?.find(
+      (itinerary) => itinerary.day === parsedDay
+    )
+
+    if (!targetItinerary) return
+
+    const updatedItinerary =
+      formData.productItinerary?.itineraries?.indexOf(targetItinerary)
+
+    if (updatedItinerary === -1) return
+
+    // Update the specific itinerary based on the type (icon, image, content)
+    const updatedItineraries = [
+      ...(formData.productItinerary?.itineraries as ItineraryItem[]).slice(
+        0,
+        updatedItinerary
+      ),
+      {
+        ...targetItinerary,
+        [type]: value,
+      },
+      ...(formData.productItinerary?.itineraries as ItineraryItem[]).slice(
+        (updatedItinerary as number) + 1
+      ),
+    ]
+
+    setFormData({
+      ...formData,
+      productItinerary: {
+        ...formData.productItinerary,
+        itineraries: updatedItineraries,
+      },
+    })
+  }
 
   return (
     <Grid container direction="row" spacing={4} size={{ xs: 12, md: 6 }}>
@@ -173,7 +271,115 @@ export const Form = () => {
           />
         </Grid>
         <Grid>
-          <Stack direction="row" spacing={3}>
+          {formData.productItinerary?.summary?.length ||
+          formData.productItinerary?.itineraries?.length ? (
+            <Typography variant="h4" mb={2}>
+              Itineraries
+            </Typography>
+          ) : null}
+          {formData.productItinerary?.itineraries?.map((itinerary, idx) => (
+            <Stack
+              key={itinerary.day}
+              spacing={2}
+              mb={2}
+              border="1px solid lightgrey"
+              borderRadius={2}
+              p={2.5}
+            >
+              <Stack
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <Typography variant="h5">Day {itinerary.day}</Typography>
+                <DeleteIcon
+                  color="error"
+                  onClick={() => removeItinerary(itinerary.day)}
+                  sx={{ cursor: "pointer" }}
+                />
+              </Stack>
+              <Stack
+                mb={
+                  formData.productItinerary?.itineraries &&
+                  formData.productItinerary?.itineraries?.length >= 1
+                    ? 2
+                    : 0
+                }
+                spacing={2}
+              >
+                <FormControl fullWidth>
+                  <InputLabel id="icon-label">Icon</InputLabel>
+                  <Select
+                    fullWidth
+                    name={`icon-${itinerary.day}`}
+                    labelId="icon-label"
+                    value={itinerary.icon}
+                    label="Icon"
+                    onChange={(e: SelectChangeEvent<string>) =>
+                      handleItineraryChange(e)
+                    }
+                    startAdornment={
+                      itinerary.icon ? (
+                        <InputAdornment position="start">
+                          {PRODUCT_ICONS[itinerary.icon as string]}
+                        </InputAdornment>
+                      ) : null
+                    }
+                  >
+                    <MenuItem value="">Select icon</MenuItem>
+                    {Object.values(PRODUCT_TYPES).map((type) => (
+                      <MenuItem key={type} value={type.toLowerCase()}>
+                        {type}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+                <TextField
+                  fullWidth
+                  name={`image-${itinerary.day}`}
+                  label="Image URL"
+                  aria-label="Image URL"
+                  placeholder="https://picsum.photos/200"
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    handleItineraryChange(e)
+                  }
+                  value={formData.productItinerary?.itineraries?.[idx].image}
+                />
+                {itinerary.image && (
+                  <Image
+                    src={itinerary.image}
+                    width='200'
+                    height='100'
+                    alt={`Day ${itinerary.day} image`}
+                  />
+                )}
+                <TextField
+                  fullWidth
+                  name={`content-${itinerary.day}`}
+                  label={`Day ${itinerary.day} content`}
+                  aria-label={`Day ${itinerary.day} content`}
+                  placeholder={`Departing from city`}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    handleItineraryChange(e)
+                  }
+                  value={formData.productItinerary?.itineraries?.[idx].content}
+                />
+              </Stack>
+            </Stack>
+          ))}
+          <Button variant="contained" color="success" onClick={addItinerary}>
+            Add itinerary
+          </Button>
+        </Grid>
+        <Grid>
+          <Stack
+            direction="row"
+            spacing={3}
+            border="1px solid lightgrey"
+            borderRadius={2}
+            p={2.5}
+            mt={4}
+          >
             <Button variant="contained" onClick={handleGenerateJson}>
               Generate JSON!
             </Button>
